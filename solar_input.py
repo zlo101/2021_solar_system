74d551f90fdb0a676ef8c0480def35866e6a7deb
Stalker165 <minaev.artem@phystech.edu>
2021-11-13 16:27:49 +0300

    solar_input.py, solar_main.py, solar_model.py, solar_vis.py updated: image can be created

--- solar_input.py
+++ solar_input.py
@@ -4,8 +4,16 @@
 from solar_objects import Star, Planet
 
 
+def E_reader(str):
+    """Обрабатывает число, в котором содержится E для обозначения степени 10, в приемлимый для компилятора вид"""
+    if not str.isnumeric:
+        return float(str.split('E')[0]) * (10 ** float(str.split('E')[1]))
+    else:
+        return float(str)
+
+
 def read_space_objects_data_from_file(input_filename):
-    """Cчитывает данные о космических объектах из файла, создаёт сами объекты
+    """Считывает данные о космических объектах из файла, создаёт сами объекты
     и вызывает создание их графических образов
 
     Параметры:
@@ -19,10 +27,14 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -44,7 +56,14 @@ def parse_star_parameters(line, star):
     **star** — объект звезды.
     """
 
-    pass  # FIXME: not done yet
+    star.R = int(line.split()[1])
+    star.color = line.split()[2]
+    star.m = E_reader(line.split()[3])
+    star.x = E_reader(line.split()[4])
+    star.y = E_reader(line.split()[5])
+    star.Vx = E_reader(line.split()[6])
+    star.Vy = E_reader(line.split()[7])
+
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,7 +80,13 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    planet.R = int(line.split()[1])
+    planet.color = line.split()[2]
+    planet.m = E_reader(line.split()[3])
+    planet.x = E_reader(line.split()[4])
+    planet.y = E_reader(line.split()[5])
+    planet.Vx = E_reader(line.split()[6])
+    planet.Vy = E_reader(line.split()[7])
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -78,9 +103,7 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
             print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME: should store real values
 
-# FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
--- solar_model.py
+++ solar_model.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 # license: GPLv3
 
+import math
+
 gravitational_constant = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
 
@@ -19,8 +21,10 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        alpha = math.atan2(body.y-obj.y, body.x - obj.x)
+        F = gravitational_constant * body.m * obj.m / r**2
+        body.Fx += F * math.cos(alpha)  # FIXME: нужно вывести формулу...
+        body.Fy += F * math.sin(alpha)
 
 
 def move_space_object(body, dt):
@@ -32,9 +36,11 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
+    ay = body.Fy/body.m
+    body.x += body.Vx*dt  # FIXME: не понимаю как менять...
     body.Vx += ax*dt
-    # FIXME: not done recalculation of y coordinate!
+    body.y += body.Vy*dt
+    body.Vy += ay*dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
